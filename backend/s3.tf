0d43f3749cabd9a7f912a15a96d0137880f9c695
Katie <keblack789@gmail.com>
2022-04-24 23:27:11 -0400

    updated to use resources for s3 bucket attributes

--- backend/s3.tf
+++ backend/s3.tf
@@ -1,19 +1,5 @@
 resource "aws_s3_bucket" "terraform-state" {
  bucket = "quest-tfstate"
- acl    = "private"
-
- versioning {
-   enabled = true
- }
-
- server_side_encryption_configuration {
-   rule {
-     apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
-       sse_algorithm     = "aws:kms"
-     }
-   }
- }
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
@@ -23,4 +9,27 @@ resource "aws_s3_bucket_public_access_block" "block" {
  block_public_policy     = true
  ignore_public_acls      = true
  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.terraform-state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.terraform-state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
